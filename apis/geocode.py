1651cc59b042d0070a65707c48dbdf2e55808979
Shorouk AbuBatta <44466523+shorouk285@users.noreply.github.com>
2019-12-10 16:26:07 +0200

    Uploading apis

---
+++ apis/geocode.py
@@ -0,0 +1,13 @@
+import httplib2
+import json
+
+
+def getGeocodeLocation(inputString):
+    google_api_key = "AIzaSyCluVT5yELlVqmZEdOrBRub3CkKzFfVobw"
+    locationString = inputString.replace(" ", "+")
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    latitude = result['results'][0]['geometry']['location']['lat']
+    longitude = result['results'][0]['geometry']['location']['lng']
+    return (latitude, longitude)
