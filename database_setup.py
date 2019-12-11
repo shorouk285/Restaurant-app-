bc8097c308745767383016ff61c53d605f0a3913
Shorouk AbuBatta <44466523+shorouk285@users.noreply.github.com>
2019-12-11 05:36:12 +0200

    Adding libraries

--- database_setup.py
+++ database_setup.py
@@ -2,6 +2,8 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+
 
 Base = declarative_base()
 
