375bef01c470f342b510e91fab4ef2e84b722118
Shorouk AbuBatta <44466523+shorouk285@users.noreply.github.com>
2019-12-11 02:46:39 +0200

    adding views api

---
+++ apis/views.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+from findARestaurant import findARestaurant
+from models import Base, Restaurant
+from flask import Flask, jsonify, request
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+import sys
+import codecs
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf8')(sys.stderr)
+
+
+
+
+foursquare_client_id = "TSJYJL4NYJPNKRRXTPF40DBOP34D5WJLBTXSUYEQOIDZCHDJ"
+foursquare_client_secret = "BYES1GT04AIEYRTDHXK1KLFBZ5KGTLI4WKB2Z4IN5KVAXZCK"
+google_api_key = "AIzaSyCluVT5yELlVqmZEdOrBRub3CkKzFfVobw"
+
+engine = create_engine('sqlite:///restaurants.db')
+
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+app = Flask(__name__)
+
+@app.route('/restaurants', methods = ['GET', 'POST'])
+def all_restaurants_handler():
+  #YOUR CODE HERE
+
+@app.route('/restaurants/<int:id>', methods = ['GET','PUT', 'DELETE'])
+def restaurant_handler(id):
+  #YOUR CODE HERE
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
